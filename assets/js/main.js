87493be5de57234c41be0f4a97ff9cba74468f18
AlvaroArbaiza <122307217+AlvaroArbaiza@users.noreply.github.com>
2023-03-23 17:10:28 +0100

    Inserimento regole javascript da far apparire nel output Biglietto

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,22 +19,31 @@ Il recap dei dati e l'output del prezzo finale, andranno quindi stampati in pagi
 
 function myFunction() { 
 
+    // nome e cognome
+    let nomeCognome = document.getElementById("nomeCognome").value;
+
     // chilometri
     let chilometri = parseFloat( document.getElementById("chilometri").value );
 
     // età
     let eta = parseInt( document.getElementById("eta").value );
 
+    // carrozza
+    let carrozza = Math.floor( (Math.random() * 10) + 1 );
+
+    // codice CP
+    let codiceCP = Math.floor( (Math.random() * 100000) + 1 );
+
     // tariffa in base al numero di chilometri
     const tariffa = 0.21;
 
     // prezzo totale
-    let prezzoTotale = tariffa * chilometri;
+    let prezzoNormale = tariffa * chilometri;
 
     // sconto del 20%
     if ( eta < 18 ) {
 
-        scontoMinori =  prezzoTotale * 0.8;
+        scontoMinori =  prezzoNormale * 0.8;
         prezzoFinale = `
             <p>Complimenti hai uno sconto del 20&#37; perch&#233; sei minorenne!</p>  
             <span>${ ( scontoMinori.toFixed(2) ) }&#8364;</span>
@@ -43,18 +52,54 @@ function myFunction() {
     // sconto del 40%
     } else if ( eta > 65 ) {
 
-        scontoAnziani = prezzoTotale * 0.6;
+        scontoAnziani = prezzoNormale * 0.6;
         prezzoFinale = `
         <p>Complimenti hai uno sconto del 40&#37; perch&#233; sei over 65!</p>  
         <span>${ ( scontoAnziani.toFixed(2) ) }&#8364;</span>
         `;
 
     } else { 
-        prezzoFinale = prezzoTotale;
+        prezzoFinale = prezzoNormale;
     }
 
     // Inserimento dei dati nell'output
-    document.getElementById("prezzoFinale").innerHTML = prezzoFinale;
+    document.getElementById("bigliettoPasseggero").innerHTML = `
+
+         <h3>Dettagli passeggero</h3>
+
+         <div class="row">
+
+            <div class="col-4">
+                <h3 class="text-uppercase">nome passeggero</h3>
+                <h4>${nomeCognome}</h4>
+            </div>
+
+            <div class="col-2">
+
+                <h5>Classe</h5>
+                <span>${classe.value}</span>
+            </div>
+
+            <div class="col-2">
+
+                <h5>Carrozza</h5>
+                <span>${carrozza}</span>
+            </div>
+
+            <div class="col-2">
+
+                <h5>Codice CP</h5>
+                <span>${codiceCP}</span>
+            </div>
+
+            <div class="col-2">
+
+                <h5>Costo Biglietto</h5>
+                <span>${prezzoFinale}</span>
+            </div>
+         </div>
+    
+    ` ;
 
     console.log(prezzoTotale);
 
